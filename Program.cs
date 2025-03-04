1d185fba369f5fb98c93babf8cf046d6dd8b6b61
Zoey Hamm <339622581@GoTVDSB.ca>
2025-03-04 14:08:45 -0500

    Finished?

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+           
         public static void Lose(double bet, double balance)
         {
             Console.ForegroundColor = ConsoleColor.DarkGray;
@@ -10,18 +11,19 @@
             Console.WriteLine($"Your new balance is {balance.ToString("C")}. ");
         }
 
-        public static void Win(double bet, double balance)
+        public static void Win(double bet, double balance, string winnings)
         {
             Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.WriteLine("WOW! Congrats! You won your bet!");
-            Console.WriteLine($"You bet {bet.ToString("C")} and gained it back!");
+            Console.WriteLine($"You bet {bet.ToString("C")} and gained {winnings}it back!");
             Console.WriteLine($"Your new balance is {balance.ToString("C")}. ");
 
         }
         static void Main(string[] args)
         {
+            Console.Title = "Casino Italiano";
             double bet, accountBalance;
-            string choice;
+            string choice, winnings;
             int sumDies, round;
             accountBalance = 100;
             Boolean done = false;
@@ -30,7 +32,7 @@
             die2 = new Die();
             round = 1;
             
-            // NEED TO APPLY BET THINGS FROM WEBSITE (ie double bet, 1/2 bet, etc)
+
             Console.WriteLine("Hello and Welcome to Casino Italiano! Here you can bet on the outcome of two dice. \nPlease wait for the game to begin...");
             Thread.Sleep(4000);
             while (!done)
@@ -38,26 +40,30 @@
                 Console.ForegroundColor = ConsoleColor.DarkCyan;
 
                 Console.Clear();
+                winnings = "";
                 die1.RollDie();
                 die2.RollDie();
                 Console.WriteLine($"Current round: {round}.");
                 Console.WriteLine($"Your current account balance is: {accountBalance.ToString("C")} ");
                 Console.WriteLine();
-                Console.WriteLine("The possible outcomes you can bet on are: ");
-                Console.WriteLine("1 - Doubles");
-                Console.WriteLine("2 - Not Doubles");
-                Console.WriteLine("3 - Even Sum");
-                Console.WriteLine("4 - Odd Sum");
-                Console.WriteLine("Q - Quit");
+                Console.WriteLine("The possible outcomes you can bet on are, how much you would gain back, and the odds: ");
+                Console.WriteLine();
+                Console.WriteLine("1 - Doubles          [Win double your bet!]      Odds: 17%");
+                Console.WriteLine("2 - Not Doubles      [Win half your bet!]        Odds: 83%");
+                Console.WriteLine("3 - Even Sum         [Win your bet!]             Odds: 50%");
+                Console.WriteLine("4 - Odd Sum          [Win your bet!]             Odds: 50%");
+                Console.WriteLine("5 - Sum of Seven     [Win triple your bet!]      Odds: 17%");
+                Console.WriteLine("Q - Quit             [Quit the game]");
                 Console.WriteLine();
             
-                Console.WriteLine("What would you like to bet on? Type the number OR the name: ");
+                Console.WriteLine("What would you like to bet on? Type the number OR the name: \n**Incorrect selections will result in a loss of your bet**. ");
                 Console.ForegroundColor = ConsoleColor.White;
                 choice = Console.ReadLine().ToLower().Trim();
 
                 if (choice == "q" || choice == "quit")
-                {
+                { 
                     Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine();
                     Console.WriteLine("Thanks for playing!");
                     Thread.Sleep(2500);
                     done = true;
@@ -69,10 +75,10 @@
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
                     Console.WriteLine("How much money would you like to bet? ");
                     Console.ForegroundColor = ConsoleColor.White;
-                    while (!Double.TryParse(Console.ReadLine(), out bet) || bet > accountBalance || bet < 0)
+                    while (!Double.TryParse(Console.ReadLine(), out bet) || bet > accountBalance || bet < 0 || bet == 0)
                     {
                         Console.ForegroundColor = ConsoleColor.DarkCyan;
-                        Console.WriteLine($"Incorrect bet. Please try again. You can only bet up to {accountBalance.ToString("C")}.");
+                        Console.WriteLine($"Incorrect bet. Please try again. You can only bet between $1.00 and {accountBalance.ToString("C")}.");
                         Console.ForegroundColor = ConsoleColor.White;
                     }
 
@@ -87,8 +93,9 @@
                         Console.WriteLine();
                         if (die1.Roll == die2.Roll)
                         {
-                            accountBalance += bet;
-                            Win(bet, accountBalance);
+                            winnings = "double of ";
+                            accountBalance = accountBalance + (2*bet);
+                            Win(bet, accountBalance, winnings);
                             Console.WriteLine();
                             Console.ForegroundColor = ConsoleColor.DarkCyan;
                             Console.WriteLine("Press ENTER to move on to the next round! ");
@@ -113,8 +120,9 @@
                         Console.WriteLine();
                         if (die1.Roll != die2.Roll)
                         {
-                            accountBalance += bet;
-                            Win(bet, accountBalance);
+                            winnings = "half of ";
+                            accountBalance = accountBalance + (bet/2);
+                            Win(bet, accountBalance, winnings);
                             Console.WriteLine();
                             Console.ForegroundColor = ConsoleColor.DarkCyan;
                             Console.WriteLine("Press ENTER to move on to the next round! ");
@@ -141,7 +149,7 @@
                         if (sumDies % 2 == 0)
                         {
                             accountBalance += bet;
-                            Win(bet, accountBalance);
+                            Win(bet, accountBalance, winnings);
                             Console.WriteLine();
                             Console.ForegroundColor = ConsoleColor.DarkCyan;
                             Console.WriteLine("Press ENTER to move on to the next match! ");
@@ -167,7 +175,35 @@
                         if (sumDies % 2 != 0)
                         {
                             accountBalance += bet;
-                            Win(bet, accountBalance);
+                            Win(bet, accountBalance, winnings);
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            Console.WriteLine("Press ENTER to move on to the next round! ");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            accountBalance -= bet;
+                            Lose(bet, accountBalance);
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            Console.WriteLine("Press ENTER to move on to the next round! ");
+                            Console.ReadLine();
+                        }
+
+                    }
+                    else if (choice == "5" || choice == "sum of seven")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        die1.DrawDie();
+                        die2.DrawDie();
+                        Console.WriteLine();
+                        sumDies = die1.Roll + die2.Roll;
+                        if (sumDies == 7)
+                        {
+                            winnings = "triple of ";
+                            accountBalance = accountBalance + (3*bet);
+                            Win(bet, accountBalance, winnings);
                             Console.WriteLine();
                             Console.ForegroundColor = ConsoleColor.DarkCyan;
                             Console.WriteLine("Press ENTER to move on to the next round! ");
